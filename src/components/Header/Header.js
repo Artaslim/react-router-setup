be5ae13c3de306d35644288f32dee736e5147810
Artaslim <Taslimuddinar@gmail.com>
2022-10-12 19:55:00 +0600

    [header add]

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,8 +9,8 @@ const Header = () => {
           <h2 className="text-2xl font-bold  ">Quiz App</h2>
         </div>
         <div className="md:flex space-x-6 font-semibold">
-          <Link to="/topics">Topics</Link>
           <Link to="/">Home</Link>
+          <Link to="/topics">Topics</Link>
           <Link to="/blog">Blog</Link>
           <Link to="/statistics">Statistics</Link>
         </div>
