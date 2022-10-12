8bbdde4fe7e061b1571c0da78fa207130e13d070
Artaslim <Taslimuddinar@gmail.com>
2022-10-12 19:43:21 +0600

    design changed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   const { name, P, img } = home;
 
   return (
-    <div className="grid grid-cols-2 justify-items-stretch space-x-20 p-20 bg-yellow-100">
+    <div className="grid grid-cols-2 justify-evenly space-x-16 p-20 bg-yellow-100">
       <div classNam="p-14 mt-5 ml-4">
         <h2 className="text-2xl font-bold">{name}</h2>
         <p className="ml-8 mt-5">{P}</p>
